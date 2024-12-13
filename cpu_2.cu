7a0ca91e4f668e3bd706031a636dccc9f4a2003d
sunjian <sunjxan@gmail.com>
2024-12-13 19:39:37 +0800

    cpu 2

--- cpu_2.cu
+++ cpu_2.cu
@@ -6,9 +6,9 @@ void sum_matrix(const real *A, const real *B, real *C)
     const real (*nB)[N] = reinterpret_cast<decltype(nB)>(B);
     real (*nC)[N] = reinterpret_cast<decltype(nC)>(C);
 
-    for (size_t i = 0; i < N; ++i) {
-        for (size_t j = 0; j < M; ++j) {
-            nC[j][i] = nA[j][i] + nB[j][i];
+    for (size_t i = 0; i < M; ++i) {
+        for (size_t j = 0; j < N; ++j) {
+            nC[i][j] = nA[i][j] + nB[i][j];
         }
     }
 }
