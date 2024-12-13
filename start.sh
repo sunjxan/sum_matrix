1ed1a57930748066012c2b47ea2c45e981c5eae6
sunjian <sunjxan@gmail.com>
2024-12-13 20:48:08 +0800

    ok

--- start.sh
+++ start.sh
@@ -9,3 +9,15 @@ nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr cuda_1.cu -o a.out
 nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cuda_1.cu -o a.out && ./a.out
 nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr cuda_2.cu -o a.out && ./a.out
 nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cuda_2.cu -o a.out && ./a.out
+echo ""
+echo "-Xptxas -dlcm=ca"
+nvcc -O2 -std=c++17 -Xcompiler -Wall -Xptxas -dlcm=ca --expt-relaxed-constexpr cuda_1.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall -Xptxas -dlcm=ca --expt-relaxed-constexpr -DUSE_DP cuda_1.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall -Xptxas -dlcm=ca --expt-relaxed-constexpr cuda_2.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall -Xptxas -dlcm=ca --expt-relaxed-constexpr -DUSE_DP cuda_2.cu -o a.out && ./a.out
+echo ""
+echo "-Xptxas -dlcm=cg"
+nvcc -O2 -std=c++17 -Xcompiler -Wall -Xptxas -dlcm=cg --expt-relaxed-constexpr cuda_1.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall -Xptxas -dlcm=cg --expt-relaxed-constexpr -DUSE_DP cuda_1.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall -Xptxas -dlcm=cg --expt-relaxed-constexpr cuda_2.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall -Xptxas -dlcm=cg --expt-relaxed-constexpr -DUSE_DP cuda_2.cu -o a.out && ./a.out
